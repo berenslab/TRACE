d4f98dc4fc44fe80a92302771b06369fc5cdd24a
Niklas Böhm <mail@jnboehm.com>
2024-12-13 10:21:30 +0100

    Format timesseries_data.py

--- timeseries_data.py
+++ timeseries_data.py
@@ -1,27 +1,37 @@
 import argparse
-from datetime import datetime
-import h5py
 import os
 import pickle
 import random
 import sys
 import time
+from datetime import datetime
 
+import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy
 import seaborn as sns
 import torch
+from cne import ContrastiveEmbedding
 from matplotlib.colors import ListedColormap
 from torch.utils.data import DataLoader
 
-from cne import ContrastiveEmbedding
-sys.path.append("/gpfs01/berens/user/lschmors/Code/superior_colliculus/cne_timeseries/")
-from sc_utils import ContrastiveTrialPairGenerator, TimeSeriesDataset, TimeSeriesMLP
+sys.path.append(
+    "/gpfs01/berens/user/lschmors/Code/superior_colliculus/cne_timeseries/"
+)
+from sc_utils import (
+    ContrastiveTrialPairGenerator,
+    TimeSeriesDataset,
+    TimeSeriesMLP,
+)
 
-def load_data(filepath='/gpfs01/berens/user/lschmors/Code/superior_colliculus', trim=True,
-                  flatten_bar=False):
+
+def load_data(
+    filepath="/gpfs01/berens/user/lschmors/Code/superior_colliculus",
+    trim=True,
+    flatten_bar=False,
+):
     """
     Load data for chirp responses and bar responses.
 
@@ -37,8 +47,8 @@ def load_data(filepath='/gpfs01/berens/user/lschmors/Code/superior_colliculus', 
 
     """
     # Load chirp trial responses
-    file_name = filepath + '/Data/retinal_axons/chirp_trials.h5'
-    with h5py.File(file_name, 'r') as file:
+    file_name = filepath + "/Data/retinal_axons/chirp_trials.h5"
+    with h5py.File(file_name, "r") as file:
         # Check if the dataset exists
         if "chirp_trials" in file:
             data_chirp = file["chirp_trials"][:]  # Shape: (ROIs, trials, time)
@@ -46,20 +56,24 @@ def load_data(filepath='/gpfs01/berens/user/lschmors/Code/superior_colliculus', 
             raise ValueError("Dataset 'chirp_trials' not found in file.")
 
     # Load bar trial responses
-    file_name_bar = filepath + '/Data/retinal_axons/bar_trials.h5'
-    with h5py.File(file_name_bar, 'r') as file:
+    file_name_bar = filepath + "/Data/retinal_axons/bar_trials.h5"
+    with h5py.File(file_name_bar, "r") as file:
         # Check if the dataset exists
         if "bar_trials" in file:
             data_bar = file["bar_trials"][:]  # Shape: (ROIs, trials, time)
         else:
             raise ValueError("Dataset 'bar_trials' not found in file.")
-    if flatten_bar==False:
+    if flatten_bar == False:
         # Get temporal response only
-        data_bar_non_norm = np.mean(data_bar, axis=2) # [ROIs, trials, 2sec]
+        data_bar_non_norm = np.mean(data_bar, axis=2)  # [ROIs, trials, 2sec]
     else:
-        data_bar_non_norm = data_bar.reshape([data_bar.shape[0],
-                                             data_bar.shape[1],
-                                             data_bar.shape[2]*data_bar.shape[3]]) # [ROIs, trials, 8*2sec]
+        data_bar_non_norm = data_bar.reshape(
+            [
+                data_bar.shape[0],
+                data_bar.shape[1],
+                data_bar.shape[2] * data_bar.shape[3],
+            ]
+        )  # [ROIs, trials, 8*2sec]
     # Normalize data
     data_chirp_norm = normalize_data(data_chirp)
     data_bar_norm = normalize_data(data_bar_non_norm)
@@ -69,11 +83,14 @@ def load_data(filepath='/gpfs01/berens/user/lschmors/Code/superior_colliculus', 
         data_chirp_norm = data_chirp_norm[:, :, 9:]
 
     # Load labels
-    file_name = filepath + '/20240207_tSNE/data/20240207_df_clusterd_identified.pkl'
+    file_name = (
+        filepath + "/20240207_tSNE/data/20240207_df_clusterd_identified.pkl"
+    )
     df_clustered = pd.read_pickle(file_name)
-    labels = df_clustered['clusterID_sorted'].values.astype(int)
+    labels = df_clustered["clusterID_sorted"].values.astype(int)
 
     return data_chirp_norm, data_bar_norm, labels
+
 
 def normalize_data(data):
     """
@@ -93,46 +110,91 @@ def normalize_data(data):
     unit_std = np.std(unit_average, axis=1)
 
     # Normalize each unit's data across all trials using the grand mean and grand SD
-    data_normalized = (data - unit_mean[:, np.newaxis, np.newaxis]) / unit_std[:, np.newaxis,
-                                                                      np.newaxis]
+    data_normalized = (data - unit_mean[:, np.newaxis, np.newaxis]) / unit_std[
+        :, np.newaxis, np.newaxis
+    ]
 
     return data_normalized
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--model_name", type=str, default='TimeSeriesMLP')
-    parser.add_argument("-r", "--run", type=int, help="Seed for random number generator.")
-    parser.add_argument("-e", "--epochs", default=200, type=int, help="Number of epochs.")
-    parser.add_argument("-b", "--batch_size", default=64, type=int, help="Batch size.")
-    parser.add_argument("-d", "--dir", type=str, help="Directory to save results.")
-    parser.add_argument("-o", "--output_dim", type=int, help="Dimensions of final layer.")
-    parser.add_argument("-l", "--loss_mode", type=str, default='infonce', help="Mode of the loss.")
-    parser.add_argument("-tc", "--trials_chirp", type=int, default=5,
-                        help="Number of trials to average over for pos pairs chirp.")
-    parser.add_argument("-tb", "--trials_bar", type=int, default=5,
-                        help="Number of trials to average over for pos pairs bar.")
-    parser.add_argument("-a", "--augmentations", action="store_true",
-                        help="Use common data augmentations instead of partial means.")
-    parser.add_argument("-fb", "--flatten_bar", action="store_true",
-                        help="Flatten responses 8 directions or use mean across directions.")
+    parser.add_argument(
+        "-m", "--model_name", type=str, default="TimeSeriesMLP"
+    )
+    parser.add_argument(
+        "-r", "--run", type=int, help="Seed for random number generator."
+    )
+    parser.add_argument(
+        "-e", "--epochs", default=200, type=int, help="Number of epochs."
+    )
+    parser.add_argument(
+        "-b", "--batch_size", default=64, type=int, help="Batch size."
+    )
+    parser.add_argument(
+        "-d", "--dir", type=str, help="Directory to save results."
+    )
+    parser.add_argument(
+        "-o", "--output_dim", type=int, help="Dimensions of final layer."
+    )
+    parser.add_argument(
+        "-l",
+        "--loss_mode",
+        type=str,
+        default="infonce",
+        help="Mode of the loss.",
+    )
+    parser.add_argument(
+        "-tc",
+        "--trials_chirp",
+        type=int,
+        default=5,
+        help="Number of trials to average over for pos pairs chirp.",
+    )
+    parser.add_argument(
+        "-tb",
+        "--trials_bar",
+        type=int,
+        default=5,
+        help="Number of trials to average over for pos pairs bar.",
+    )
+    parser.add_argument(
+        "-a",
+        "--augmentations",
+        action="store_true",
+        help="Use common data augmentations instead of partial means.",
+    )
+    parser.add_argument(
+        "-fb",
+        "--flatten_bar",
+        action="store_true",
+        help="Flatten responses 8 directions or use mean across directions.",
+    )
     args = parser.parse_args()
 
     # Load data and create dataset
     data_chirp, data_bar, labels = load_data(flatten_bar=args.flatten_bar)
     if args.augmentations:
         if args.flatten_bar:
-            noise_samples = np.load('/gpfs01/berens/user/lschmors/Code/superior_colliculus/'
-                                    '20241016_data_augmentations/noise_samples_flattenbarTrue.npy')
+            noise_samples = np.load(
+                "/gpfs01/berens/user/lschmors/Code/superior_colliculus/"
+                "20241016_data_augmentations/noise_samples_flattenbarTrue.npy"
+            )
         else:
-            noise_samples = np.load('/gpfs01/berens/user/lschmors/Code/superior_colliculus/'
-                                    '20241016_data_augmentations/noise_samples.npy')
+            noise_samples = np.load(
+                "/gpfs01/berens/user/lschmors/Code/superior_colliculus/"
+                "20241016_data_augmentations/noise_samples.npy"
+            )
     else:
         noise_samples = None
-    dataset = ContrastiveTrialPairGenerator(data_chirp, data_bar,
-                                            n_trials_pos_pair_chirp=int(args.trials_chirp),
-                                            n_trials_pos_pair_bar=int(args.trials_bar),
-                                            data_aug=args.augmentations,
-                                            noise_samples=noise_samples)
+    dataset = ContrastiveTrialPairGenerator(
+        data_chirp,
+        data_bar,
+        n_trials_pos_pair_chirp=int(args.trials_chirp),
+        n_trials_pos_pair_bar=int(args.trials_bar),
+        data_aug=args.augmentations,
+        noise_samples=noise_samples,
+    )
 
     # Set parameters
     model_name = str(args.model_name)
@@ -142,13 +204,13 @@ def main():
     gen = torch.Generator().manual_seed(run)
     num_workers = 8
 
-    batch_size = int(args.batch_size) #1024
+    batch_size = int(args.batch_size)  # 1024
     negative_samples = 10  # example, adjust based on your requirements
 
     # ContrastiveEmbedding params
     n_epochs = int(args.epochs)
     device = "cuda:0"
-    learning_rate = 0.15 * batch_size #0.03 * batch_size / 256
+    learning_rate = 0.15 * batch_size  # 0.03 * batch_size / 256
     lr_min_factor = 0.0
     momentum = 0.0
     temperature = 0.5
@@ -177,12 +239,14 @@ def main():
     # Saving directory
     current_datetime = datetime.now()
     datetime_string = current_datetime.strftime("%Y%m%d_%H%M%S")
-    file_name = f"{datetime_string}_embd_{model_name}_epochs{n_epochs}_batchsize{batch_size}"\
-                f"_outputdim{output_dim}_run{run}_ntrialsc{int(args.trials_chirp)}"\
-                f"_ntrialsb{int(args.trials_bar)}_lossmode{args.loss_mode}" \
-                f"_flattenbar{str(args.flatten_bar)}_dataug{args.augmentations}"
-    print(f'Directory: {args.dir}')
-    print(f'File name: {file_name}')
+    file_name = (
+        f"{datetime_string}_embd_{model_name}_epochs{n_epochs}_batchsize{batch_size}"
+        f"_outputdim{output_dim}_run{run}_ntrialsc{int(args.trials_chirp)}"
+        f"_ntrialsb{int(args.trials_bar)}_lossmode{args.loss_mode}"
+        f"_flattenbar{str(args.flatten_bar)}_dataug{args.augmentations}"
+    )
+    print(f"Directory: {args.dir}")
+    print(f"File name: {file_name}")
     plots_dir = os.path.join(args.dir, "plots")
     os.makedirs(plots_dir, exist_ok=True)
     models_dir = args.dir
@@ -197,8 +261,10 @@ def main():
         generator=gen,
         drop_last=True,
     )
-    model = TimeSeriesMLP(input_features=data_chirp.shape[2]+data_bar.shape[2],
-                          n_features=output_dim)
+    model = TimeSeriesMLP(
+        input_features=data_chirp.shape[2] + data_bar.shape[2],
+        n_features=output_dim,
+    )
     cne = ContrastiveEmbedding(
         model=model,
         batch_size=batch_size,
@@ -241,7 +307,7 @@ def main():
     filepath = os.path.join(models_dir, f"{file_name}.pkl")
     with open(filepath, "wb") as file:
         pickle.dump(cne, file, pickle.HIGHEST_PROTOCOL)
-        print('Model saved as: ', filepath)
+        print("Model saved as: ", filepath)
 
     # Save loss plot
     loss_mean_per_epoch = []
@@ -251,13 +317,18 @@ def main():
     # Plotting
     fig, ax = plt.subplots(1, 1, figsize=(5, 3))
     ax.plot(loss_mean_per_epoch)
-    ax.set_xlabel('Epoch')
-    ax.set_ylabel('Loss')
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Loss")
     sns.despine()
     filepath = os.path.join(plots_dir, f"{file_name}_loss_over_epochs.png")
-    fig.patch.set_facecolor('white')
-    fig.savefig(filepath, dpi=150, bbox_inches='tight', facecolor=fig.get_facecolor(),
-                transparent=False)
+    fig.patch.set_facecolor("white")
+    fig.savefig(
+        filepath,
+        dpi=150,
+        bbox_inches="tight",
+        facecolor=fig.get_facecolor(),
+        transparent=False,
+    )
 
     # Save embeddings
     dataset_embd = TimeSeriesDataset(data_chirp=data_chirp, data_bar=data_bar)
@@ -274,11 +345,13 @@ def main():
         for batch in loader_embd:
             input_data = batch
             if isinstance(input_data, tuple):
-                input_data = input_data[0]  # Handle case when dataset returns (data, label)
+                input_data = input_data[
+                    0
+                ]  # Handle case when dataset returns (data, label)
             # Ensure input_data has batch and channel dimensions
             if input_data.ndim == 2:
                 input_data = input_data.unsqueeze(1)  # Add channel dimension
-            input_data = input_data.to('cuda')
+            input_data = input_data.to("cuda")
             z = cne.model(input_data)
             embedding.extend(z.cpu().numpy())  # Append embeddings
     embedding = np.array(embedding)
@@ -293,9 +366,15 @@ def main():
     ax.set_yticklabels([])
     sns.despine(bottom=True, left=True)
     filepath = os.path.join(plots_dir, f"{file_name}_embedding.png")
-    fig.patch.set_facecolor('white')
-    fig.savefig(filepath, dpi=150, bbox_inches='tight', facecolor=fig.get_facecolor(),
-                transparent=False)
+    fig.patch.set_facecolor("white")
+    fig.savefig(
+        filepath,
+        dpi=150,
+        bbox_inches="tight",
+        facecolor=fig.get_facecolor(),
+        transparent=False,
+    )
+
 
 if __name__ == "__main__":
     main()
