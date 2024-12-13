43c2d171563ddae83d86a6fca82c657362acda0f
Niklas Böhm <mail@jnboehm.com>
2024-12-13 10:57:15 +0100

    Rewrite dataloaders for using variable number of trial datasets

--- sc_utils.py
+++ sc_utils.py
@@ -14,19 +14,15 @@ from data_aug import AmpJitter, Noise, TempJitter
 class TimeSeriesDataset(Dataset):
     def __init__(
         self,
-        data_chirp,
-        data_bar,
+        datasets,
         labels=None,
         transform=None,
         noise_samples=None,
     ):
         """
         Args:
-            data_chirp (np.array):
-                Responses to chirp stimulus.
-                Time series data of shape (num_samples, num_trials, num_features).
-            data_bar (np.array):
-                Responses to moving bar stimulus.
+            datasets (np.array or list):
+                List of datasets, e.g. Responses to chirp stimulus and moving bar stimulus.
                 Time series data of shape (num_samples, num_trials, num_features).
             labels (np.array):
                 Labels for the data retrieved from clustering.
@@ -34,19 +30,26 @@ class TimeSeriesDataset(Dataset):
                 Optional transform to be applied on a sample instead of generating pairs
                 using random sub-samples of trials.
         """
-        self.data_chirp = data_chirp
-        self.data_bar = data_bar
+        if not isinstance(datasets, list):
+            self.datasets = [datasets]
+        else:
+            self.datasets = datasets
         self.labels = labels
         self.transform = transform
         self.noise_samples = noise_samples
 
     def __len__(self):
-        return self.data_chirp.shape[0]
+        return self.datasets[0].shape[0]
 
     def __getitem__(self, idx):
-        sample_chirp = np.mean(self.data_chirp[idx], axis=0)  # Compute mean over trials
-        sample_bar = np.mean(self.data_bar[idx], axis=0)  # Compute mean over trials
-        sample = np.concatenate([sample_chirp, sample_bar])
+        sample_chirp = np.mean(
+            self.data_chirp[idx], axis=0
+        )  # Compute mean over trials
+        sample_bar = np.mean(
+            self.data_bar[idx], axis=0
+        )  # Compute mean over trials
+        samples = [np.mean(ds[idx], axis=0) for ds in self.datasets]
+        sample = np.concatenate(samples)
         if self.transform:
             sample = self.transform(sample)
         if self.labels is not None:
@@ -61,76 +64,108 @@ class ContrastiveTrialPairGenerator(Dataset):
     For the dynamic trial sampling, each pair is newly computed by selecting random trials
     for mean computation.
     """
-    def __init__(self, dataset_chirp, dataset_bar,
-                 n_trials_pos_pair_chirp=7,
-                 n_trials_pos_pair_bar=5,
-                 data_aug=False,
-                 noise_samples=None,
-                 ):
+
+    def __init__(
+        self,
+        trials,
+        n_trials_pp=[7, 5],
+        data_aug=False,
+        noise_samples=None,
+    ):
         """
         Args:
             dataset_chirp (np.ndarray): Chirp stimulus responses
             dataset_bar (np.ndarray): Bar stimulus responses
-            n_trials_pos_pair_chirp (int): Trials to average for chirp positive pairs
-            n_trials_pos_pair_bar (int): Trials to average for bar positive pairs
+            n_trials_pp_pos_pair_chirp (int): Trials to average for chirp positive pairs
+            n_trials_pp_pos_pair_bar (int): Trials to average for bar positive pairs
             data_aug (bool): Whether to use data augmentations
-            noise_samples (np.ndarray): precomputed noise samples based on the
+            noise_samples (np.ndarray): precomputed noise samples based on the covariance matrix
         """
-        # Chirp
-        self.dataset_chirp = dataset_chirp
-        self.num_trials_chirp = dataset_chirp.shape[1]  # number of trials recorded
-        self.ntrials_chirp = n_trials_pos_pair_chirp  # trials to average over for pos. pair
+        if not isinstance(trials, list):
+            self.trials = [trials]
+        else:
+            self.trials = trials
 
-        # Moving bar
-        self.dataset_bar = dataset_bar
-        self.num_trials_bar = dataset_bar.shape[1]
-        self.ntrials_bar = n_trials_pos_pair_bar
+        if isinstance(n_trials_pp, list) and len(n_trials_pp) != len(self.trials):
+            warnings.warn(
+                f"Got {len(self.trials)} datasets, but got {len(n_trials_pp)=}"
+            )
+            self.n_trials_pp = n_trials_pp[: len(self.trials)]
+
+        # self.dataset_chirp = dataset_chirp
+        # self.num_trials_chirp = dataset_chirp.shape[
+        #     1
+        # ]  # number of trials recorded
+        # self.ntrials_chirp = (
+        #     n_trials_pos_pair_chirp  # trials to average over for pos. pair
+        # )
+
+        # # Moving bar
+        # self.dataset_bar = dataset_bar
+        # self.num_trials_bar = dataset_bar.shape[1]
+        # self.ntrials_bar = n_trials_pos_pair_bar
 
         # Apply data augmentations True/False
         if data_aug:
-            assert noise_samples is not None, 'Please provide noise for noise augmentation'
+            assert (
+                noise_samples is not None
+            ), "Please provide noise for noise augmentation"
         self.data_aug = data_aug
         self.noise_samples = noise_samples
 
+        if self.data_aug:
+            self.trials_mean = [ds.mean(axis=1) for ds in self.trials]
+        else:
+            # Use random sub-sample of trials to generate positive pair
+            assert all(n_trial_sample <= ds.shape[1] / 2 for zip(self.trials, self.n_trials_pp)) , (
+                "Not enough trials to average over for generating positive pair. "
+                "Please choose a smaller n_trials_pp"
+            )
+
+            # n_time = sum(ds.shape[1] for ds in self.trials)
+            # self.sample1 = np.empty(shape=n_time)
+            # self.sample2 = np.empty(shape=n_time)
+            self.samples1 = [] * len(self.trials)
+            self.samples2 = [] * len(self.trials)
+
     def __len__(self):
-        return self.dataset_chirp.shape[0]
+        return self.trials[0].shape[0]
 
     def __getitem__(self, idx):
         if self.data_aug:
             # Use data augmentations to generate positive pair
             self.transform = get_transforms(noise_samples=self.noise_samples)
 
-            item_chirp = np.mean(self.dataset_chirp[idx, :, :], axis=0)
-            item_bar = np.mean(self.dataset_bar[idx, :, :], axis=0)
-            item = np.concatenate([item_chirp, item_bar])
+            items = [ds[idx] for ds in self.trials_mean]
+            item = np.concatenate(items)
 
             sample1 = self.transform(item)
             sample2 = self.transform(item)
         else:
-            # Use random sub-sample of trials to generate positive pair
-            assert ((self.ntrials_chirp <= self.num_trials_chirp / 2) or
-                    (self.ntrials_bar <= self.num_trials_bar / 2)), \
-                'Not enough trials to average over for generating positive pair. ' \
-                'Please choose a smaller n_trials_pos_pair'
 
-            # Generate positive pair for chirp response
-            n_pos_trials_chirp = self.ntrials_chirp
-            num_trials_chirp = self.num_trials_chirp
-            trials_indices1_chirp, trials_indices2_chirp = self._generate_dynamic_pairs_indices(
-                n_pos_trials_chirp, num_trials_chirp)
-            sample1_chirp = np.mean(self.dataset_chirp[idx, trials_indices1_chirp, :], axis=0)
-            sample2_chirp = np.mean(self.dataset_chirp[idx, trials_indices2_chirp, :], axis=0)
-
-            # Generate positive pair for moving bar responses
-            n_pos_trials_bar = self.ntrials_bar
-            num_trials_bar = self.num_trials_bar
-            trials_indices1_bar, trials_indices2_bar = self._generate_dynamic_pairs_indices(
-                n_pos_trials_bar, num_trials_bar)
-            sample1_bar = np.mean(self.dataset_bar[idx, trials_indices1_bar, :], axis=0)
-            sample2_bar = np.mean(self.dataset_bar[idx, trials_indices2_bar, :], axis=0)
-
-            sample1 = np.concatenate([sample1_chirp, sample1_bar])
-            sample2 = np.concatenate([sample2_chirp, sample2_bar])
+            samples1=[]
+            samples2=[]
+            n_prev_feat = 0
+            for i, (ds, n_trial_pp) in enumerate(zip(self.trials, self.n_trials_pp)):
+                # Generate positive pair
+                trial_indices1, trial_indices2 = (
+                    self._generate_dynamic_pairs_indices(
+                        n_trial_pp, ds.shape[1]
+                    )
+                )
+                sample1_ = np.mean(
+                    ds[idx, trial_indices1, :], axis=0
+                )
+                self.samples1[i] = sample1_
+                # self.sample1[n_prev_feat:sample1_.shape[-1]] = sample1_
+                sample2_ = np.mean(
+                    ds[idx, trial_indices2, :], axis=0
+                )
+                self.samples2[i] = sample2_
+                # self.sample2[n_prev_feat:sample2_.shape[-1]] = sample2_
+                # n_prev_feat +=
+            sample1 = np.concat(self.samples1)
+            sample2 = np.concat(self.samples2)
 
         return sample1, sample2
 
