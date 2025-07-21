40b8f18091bef71b5c52b13756cdca6a49206cee
Sebastian Damrich <sebastian.damrich@gmx.de>
2025-07-21 11:37:44 +0200

    update run_ts2vec_sc.py to TS2Vec+PCA and TRACE+Ts2Vec settings

--- run_ts2vec_sc.py
+++ run_ts2vec_sc.py
@@ -3,21 +3,23 @@ import numpy as np
 from ts2vec.ts2vec import TS2Vec
 import torch
 from timeseries_data import load_data_sc
+import time
 
 sc_path = "/gpfs01/berens/data/data/superior_colliculus/"
 result_path = "/gpfs01/berens/user/sdamrich/data/cl_neuro/ts2vec"
 
 
 # first hyperparameter set is the default, second is adapted to our setup
+# hyperparameters are n_epochs, batch_size, similarity type, and embedding dimension
 other_hparams = [
-    [None, 16, "cosine"],
-    [1000, 768, "cauchy"]
+    [None, 16, "cosine", 320], # TS2Vec+PCA
+    [1000, 768, "cauchy", 2] # TRACE+TS2Vec
 ]
 
-seed = 0
+seed = 2
 
-for n_epochs, batch_size, similarity in other_hparams:
-
+for n_epochs, batch_size, similarity, output_dims in other_hparams:
+    print("loading data")
     data = load_data_sc(flatten_bar=False)
 
     # compute mean trial responses
@@ -31,18 +33,22 @@ for n_epochs, batch_size, similarity in other_hparams:
     model = TS2Vec(
         input_dims=1,
         device=0,
-        output_dims=2,
+        output_dims=output_dims,
         batch_size=batch_size
 
     )
+    print("starting training")
+    start = time.time()
     loss_log = model.fit(
         mean_data[:, :, None],
         verbose=True,
         n_epochs=n_epochs,
         similarity=similarity,
     )
-    embd = model.encode(mean_data[:, :, None], encoding_window='full_series')
 
-    file_name = f"sc_dat_ts2vec_epochs_{n_epochs}_bs_{batch_size}_sim_{similarity}_seed_{seed}_embd.npy"
-    np.save(os.path.join(result_path, file_name), embd)
+    embd = model.encode(mean_data[:, :, None], encoding_window='full_series')
+    end = time.time()
+    print(f"done after {end-start} seconds")
+    file_name = f"sc_dat_ts2vec_epochs_{n_epochs}_bs_{batch_size}_sim_{similarity}_dim_{output_dims}_seed_{seed}_embd.npz"
+    np.savez(os.path.join(result_path, file_name), embd=embd, time=end-start)
     print(f"Saved {file_name}")
